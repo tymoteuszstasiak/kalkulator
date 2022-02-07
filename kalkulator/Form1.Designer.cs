60d7134e1c4b52701fe45fd417315d18acd70568
visual studio <visual.studio@drzewniak.slupsk.pl>
2022-02-07 12:16:13 +0100

    kalkulator2

--- kalkulator/Form1.Designer.cs
+++ kalkulator/Form1.Designer.cs
@@ -45,180 +45,208 @@
             this.bDzielenie = new System.Windows.Forms.Button();
             this.bWynik = new System.Windows.Forms.Button();
             this.bDEL = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // tbWynik
             // 
             this.tbWynik.Location = new System.Drawing.Point(34, 22);
             this.tbWynik.Name = "tbWynik";
-            this.tbWynik.Size = new System.Drawing.Size(366, 23);
+            this.tbWynik.Size = new System.Drawing.Size(449, 23);
             this.tbWynik.TabIndex = 0;
             // 
             // b7
             // 
+            this.b7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.b7.Location = new System.Drawing.Point(34, 77);
             this.b7.Name = "b7";
             this.b7.Size = new System.Drawing.Size(75, 47);
             this.b7.TabIndex = 1;
             this.b7.Text = "7";
-            this.b7.UseVisualStyleBackColor = true;
+            this.b7.UseVisualStyleBackColor = false;
             this.b7.Click += new System.EventHandler(this.b7_Click);
             // 
             // b8
             // 
+            this.b8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.b8.Location = new System.Drawing.Point(130, 77);
             this.b8.Name = "b8";
             this.b8.Size = new System.Drawing.Size(75, 47);
             this.b8.TabIndex = 2;
             this.b8.Text = "8";
-            this.b8.UseVisualStyleBackColor = true;
+            this.b8.UseVisualStyleBackColor = false;
             this.b8.Click += new System.EventHandler(this.b8_Click);
             // 
             // b9
             // 
+            this.b9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.b9.Location = new System.Drawing.Point(227, 77);
             this.b9.Name = "b9";
             this.b9.Size = new System.Drawing.Size(75, 47);
             this.b9.TabIndex = 3;
             this.b9.Text = "9";
-            this.b9.UseVisualStyleBackColor = true;
+            this.b9.UseVisualStyleBackColor = false;
             this.b9.Click += new System.EventHandler(this.b9_Click);
             // 
             // bDodawanie
             // 
-            this.bDodawanie.Location = new System.Drawing.Point(316, 77);
+            this.bDodawanie.BackColor = System.Drawing.Color.Gray;
+            this.bDodawanie.Location = new System.Drawing.Point(408, 77);
             this.bDodawanie.Name = "bDodawanie";
-            this.bDodawanie.Size = new System.Drawing.Size(75, 47);
+            this.bDodawanie.Size = new System.Drawing.Size(75, 122);
             this.bDodawanie.TabIndex = 4;
             this.bDodawanie.Text = "+";
-            this.bDodawanie.UseVisualStyleBackColor = true;
+            this.bDodawanie.UseVisualStyleBackColor = false;
             this.bDodawanie.Click += new System.EventHandler(this.bDodawanie_Click);
             // 
             // b4
             // 
+            this.b4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.b4.Location = new System.Drawing.Point(34, 152);
             this.b4.Name = "b4";
             this.b4.Size = new System.Drawing.Size(75, 47);
             this.b4.TabIndex = 5;
             this.b4.Text = "4";
-            this.b4.UseVisualStyleBackColor = true;
+            this.b4.UseVisualStyleBackColor = false;
             this.b4.Click += new System.EventHandler(this.b4_Click);
             // 
             // b5
             // 
+            this.b5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.b5.Location = new System.Drawing.Point(130, 152);
             this.b5.Name = "b5";
             this.b5.Size = new System.Drawing.Size(75, 47);
             this.b5.TabIndex = 6;
             this.b5.Text = "5";
-            this.b5.UseVisualStyleBackColor = true;
+            this.b5.UseVisualStyleBackColor = false;
             this.b5.Click += new System.EventHandler(this.b5_Click);
             // 
             // b6
             // 
+            this.b6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.b6.Location = new System.Drawing.Point(227, 152);
             this.b6.Name = "b6";
             this.b6.Size = new System.Drawing.Size(75, 47);
             this.b6.TabIndex = 7;
             this.b6.Text = "6";
-            this.b6.UseVisualStyleBackColor = true;
+            this.b6.UseVisualStyleBackColor = false;
             this.b6.Click += new System.EventHandler(this.b6_Click);
             // 
             // bOdejmowanie
             // 
+            this.bOdejmowanie.BackColor = System.Drawing.Color.Gray;
             this.bOdejmowanie.Location = new System.Drawing.Point(316, 152);
             this.bOdejmowanie.Name = "bOdejmowanie";
             this.bOdejmowanie.Size = new System.Drawing.Size(75, 47);
             this.bOdejmowanie.TabIndex = 8;
             this.bOdejmowanie.Text = "-";
-            this.bOdejmowanie.UseVisualStyleBackColor = true;
+            this.bOdejmowanie.UseVisualStyleBackColor = false;
             this.bOdejmowanie.Click += new System.EventHandler(this.bOdejmowanie_Click);
             // 
             // b1
             // 
+            this.b1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.b1.Location = new System.Drawing.Point(34, 225);
             this.b1.Name = "b1";
             this.b1.Size = new System.Drawing.Size(75, 47);
             this.b1.TabIndex = 9;
             this.b1.Text = "1";
-            this.b1.UseVisualStyleBackColor = true;
+            this.b1.UseVisualStyleBackColor = false;
             this.b1.Click += new System.EventHandler(this.b1_Click);
             // 
             // b2
             // 
+            this.b2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.b2.Location = new System.Drawing.Point(130, 225);
             this.b2.Name = "b2";
             this.b2.Size = new System.Drawing.Size(75, 47);
             this.b2.TabIndex = 10;
             this.b2.Text = "2";
-            this.b2.UseVisualStyleBackColor = true;
+            this.b2.UseVisualStyleBackColor = false;
             this.b2.Click += new System.EventHandler(this.b2_Click);
             // 
             // b3
             // 
+            this.b3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.b3.Location = new System.Drawing.Point(227, 225);
             this.b3.Name = "b3";
             this.b3.Size = new System.Drawing.Size(75, 47);
             this.b3.TabIndex = 11;
             this.b3.Text = "3";
-            this.b3.UseVisualStyleBackColor = true;
+            this.b3.UseVisualStyleBackColor = false;
             this.b3.Click += new System.EventHandler(this.b3_Click);
             // 
             // bMnozenie
             // 
+            this.bMnozenie.BackColor = System.Drawing.Color.Gray;
             this.bMnozenie.Location = new System.Drawing.Point(316, 225);
             this.bMnozenie.Name = "bMnozenie";
             this.bMnozenie.Size = new System.Drawing.Size(75, 47);
             this.bMnozenie.TabIndex = 12;
             this.bMnozenie.Text = "*";
-            this.bMnozenie.UseVisualStyleBackColor = true;
+            this.bMnozenie.UseVisualStyleBackColor = false;
             this.bMnozenie.Click += new System.EventHandler(this.bMnozenie_Click);
             // 
             // b0
             // 
+            this.b0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.b0.Location = new System.Drawing.Point(34, 294);
             this.b0.Name = "b0";
-            this.b0.Size = new System.Drawing.Size(75, 47);
+            this.b0.Size = new System.Drawing.Size(171, 47);
             this.b0.TabIndex = 13;
             this.b0.Text = "0";
-            this.b0.UseVisualStyleBackColor = true;
+            this.b0.UseVisualStyleBackColor = false;
             this.b0.Click += new System.EventHandler(this.b0_Click);
             // 
             // bDzielenie
             // 
-            this.bDzielenie.Location = new System.Drawing.Point(316, 294);
+            this.bDzielenie.BackColor = System.Drawing.Color.Gray;
+            this.bDzielenie.Location = new System.Drawing.Point(316, 77);
             this.bDzielenie.Name = "bDzielenie";
             this.bDzielenie.Size = new System.Drawing.Size(75, 47);
             this.bDzielenie.TabIndex = 14;
             this.bDzielenie.Text = "/";
-            this.bDzielenie.UseVisualStyleBackColor = true;
+            this.bDzielenie.UseVisualStyleBackColor = false;
             this.bDzielenie.Click += new System.EventHandler(this.bDzielenie_Click);
             // 
             // bWynik
             // 
-            this.bWynik.Location = new System.Drawing.Point(227, 294);
+            this.bWynik.BackColor = System.Drawing.Color.Cyan;
+            this.bWynik.Location = new System.Drawing.Point(408, 225);
             this.bWynik.Name = "bWynik";
-            this.bWynik.Size = new System.Drawing.Size(75, 47);
+            this.bWynik.Size = new System.Drawing.Size(75, 116);
             this.bWynik.TabIndex = 15;
             this.bWynik.Text = "=";
-            this.bWynik.UseVisualStyleBackColor = true;
+            this.bWynik.UseVisualStyleBackColor = false;
             this.bWynik.Click += new System.EventHandler(this.bWynik_Click);
             // 
             // bDEL
             // 
-            this.bDEL.Location = new System.Drawing.Point(130, 294);
+            this.bDEL.BackColor = System.Drawing.Color.Red;
+            this.bDEL.Location = new System.Drawing.Point(227, 294);
             this.bDEL.Name = "bDEL";
             this.bDEL.Size = new System.Drawing.Size(75, 47);
             this.bDEL.TabIndex = 16;
             this.bDEL.Text = "DEL";
-            this.bDEL.UseVisualStyleBackColor = true;
+            this.bDEL.UseVisualStyleBackColor = false;
             this.bDEL.Click += new System.EventHandler(this.bDEL_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(316, 294);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 47);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "C";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(449, 381);
+            this.ClientSize = new System.Drawing.Size(533, 381);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.bDEL);
             this.Controls.Add(this.bWynik);
             this.Controls.Add(this.bDzielenie);
@@ -266,5 +294,6 @@
         private Button bDzielenie;
         private Button bWynik;
         private Button bDEL;
+        private Button button1;
     }
 }
